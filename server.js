382686413699c6a72498a2fdcd7ade54a7813fe2
Meier Nino <Nino.Meier@swisscom.com>
2024-11-04 20:17:23 +0100

    const replace

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-onst express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo'); // Importiere connect-mongo für MongoDB-Sessions

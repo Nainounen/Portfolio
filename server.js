cdf8ba41a7e9367e4dc14c9c8da7e34b1a0c4796
Meier Nino <Nino.Meier@swisscom.com>
2024-11-04 20:38:38 +0100

    trust proxy aktiviert und MongoDB TLS-Option gesetzt

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
+app.enable('trust proxy');
+
 const PORT = process.env.PORT || 3000;
 
 const PASSWORD = "Nino";

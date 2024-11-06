8e28dd986feeb1cff61a7d62fb7fb3a0c62433f4
Nino Meier <home.ninomeier@gmail.com>
2024-11-06 12:40:21 +0100

    only heroku

--- deploy_to_heroku.sh
+++ deploy_to_heroku.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# API-Token direkt im Skript (Achtung: Sicherheitsrisiko!)
+HEROKU_API_KEY="HRKU-2d4f80be-cf1e-4e60-ad73-b5689fd5cd27"
+
 # Überprüfen, ob ein Kommentar übergeben wurde
 if [ -z "$1" ]; then
     echo "Bitte geben Sie einen Commit-Kommentar an."
@@ -9,19 +12,16 @@ fi
 # .gitignore-Datei umbenennen, um sie vorübergehend zu entfernen
 mv .gitignore .gitignore_backup
 
-# Push zu Heroku ohne .gitignore
-echo "Pushing zu Heroku ohne .gitignore..."
+# URL überprüfen und ausgeben
+echo "Pushing zu: https://heroku:$HEROKU_API_KEY@git.heroku.com/portfolio-nino-meier.git"
+
+# Dateien zu Heroku pushen mit einer übergebenen Commit-Message
 git add .
 git commit -m "$1"
-git push https://git.heroku.com/portfolio-nino-meier.git master
+git push https://heroku:$HEROKU_API_KEY@git.heroku.com/portfolio-nino-meier.git master
 
 # .gitignore-Datei wiederherstellen
 mv .gitignore_backup .gitignore
 
-# Push zu GitHub mit .gitignore
-echo "Pushing zu GitHub mit .gitignore..."
-git add .
-git commit -m "$1"
-git push origin master
 
-echo "Deployment abgeschlossen."
+echo "Deployment abgeschlossen. .gitignore wiederhergestellt."

bd4aa278949226967bb5ec0f9f5f3dc0f97c550a
Nino Meier <home.ninomeier@gmail.com>
2024-11-06 13:31:49 +0100

    added Github button

---
+++ deploy_to_heroku.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# API-Token direkt im Skript (Achtung: Sicherheitsrisiko!)
+HEROKU_API_KEY="HRKU-2d4f80be-cf1e-4e60-ad73-b5689fd5cd27"
+
+# Überprüfen, ob ein Kommentar übergeben wurde
+if [ -z "$1" ]; then
+    echo "Bitte geben Sie einen Commit-Kommentar an."
+    exit 1
+fi
+
+# .gitignore-Datei umbenennen, um sie vorübergehend zu entfernen
+mv .gitignore .gitignore_backup
+
+# URL überprüfen und ausgeben
+echo "Pushing zu: https://heroku:$HEROKU_API_KEY@git.heroku.com/portfolio-nino-meier.git"
+
+# Dateien zu Heroku pushen mit einer übergebenen Commit-Message
+git add .
+git commit -m "$1"
+git push https://heroku:$HEROKU_API_KEY@git.heroku.com/portfolio-nino-meier.git master
+
+# .gitignore-Datei wiederherstellen
+mv .gitignore_backup .gitignore
+
+
+echo "Deployment abgeschlossen. .gitignore wiederhergestellt."

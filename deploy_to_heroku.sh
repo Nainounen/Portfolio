873acd10650e1f689b72e3b9417871c1d4cc7e5d
Meier Nino <Nino.Meier@swisscom.com>
2024-11-06 11:01:05 +0100

    removed gitignore

---
+++ deploy_to_heroku.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Überprüfen, ob ein Kommentar übergeben wurde
+if [ -z "$1" ]; then
+    echo "Bitte geben Sie einen Commit-Kommentar an."
+    exit 1
+fi
+
+# .gitignore-Datei umbenennen, um sie vorübergehend zu entfernen
+mv .gitignore .gitignore_backup
+
+# Dateien zu Heroku pushen mit einer übergebenen Commit-Message
+git add .
+git commit -m "$1"
+git push heroku master
+
+# .gitignore-Datei wiederherstellen
+mv .gitignore_backup .gitignore
+
+echo "Deployment abgeschlossen. .gitignore wiederhergestellt."
